1af5b87915004a5075492cb3f7aa99ed8e77ae31
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-10-31 15:11:34 +0200

    replace f string with format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for a in range(97, 123):
-    print((f"{a:c}", "")[a == ord('q') or a == ord('e')], end="")
+    print(("{:c}".format(a), "")[a == ord('q') or a == ord('e')], end="")
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(99):
-    print(f'{i:d} = 0x{i:2x}')
+    print('{:d} = 0x{:2x}'.format(i, i))
