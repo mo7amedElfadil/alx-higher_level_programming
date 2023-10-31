ad671ac2b35ce8689eda37864b4f66b60d571df1
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-10-31 14:58:57 +0200

    changed f string to format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,6 @@
 import random
 number = random.randint(-10000, 10000)
 last = number % 10
-print(f"The last digit of {number:d} is {last:d} and " +
+print(f"Last digit of {number:d} is {last:d} and " +
       (("is less than 6 and not 0", "is 0")[last == 0],
        "is greater than 5")[last > 5])
