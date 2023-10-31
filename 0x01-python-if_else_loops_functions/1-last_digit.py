6e380629d86507b7e272adf3bfa58f8690ad1ba6
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-10-31 15:08:49 +0200

    fixed bug with negative mod

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
+last = number % (10, -10)[number < 0]
 print(f"Last digit of {number:d} is {last:d} and " +
       (("is less than 6 and not 0", "is 0")[last == 0],
        "is greater than 5")[last > 5])
