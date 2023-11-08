9ddd3f3b2dd46e58aebe20e54640e9f9036eb3e8
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-08 23:59:11 +0200

    replaced PyList_GetItem and Py_TYPE

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,7 +1,5 @@
 #include <Python.h>
 #include <bytesobject.h>
-
-
 /**
  * print_python_bytes- prints the basic info of a python bytes object
  * @p: pyObject
@@ -27,10 +25,12 @@ void print_python_bytes(PyObject *p)
 	printf("  trying string: %s\n", byte_string);
 	printf("  first %zu bytes: ", len);
 	for (i = 0; i < len; i++)
-		printf("%02hx ",(unsigned int)(byte_string[i] & 0xFF));
+		printf("%02hx ", (unsigned int)(byte_string[i] & 0xFF));
 	printf("\n");
 
 }
+
+
 
 /**
  * print_python_list- prints the basic info of a python list object
@@ -51,10 +51,9 @@ void print_python_list(PyObject *p)
 	printf("[*] Allocated = %zu\n", l->allocated);
 	for (i = 0; i < size; i++)
 	{
-		printf("Element %zu: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
-		if (PyBytes_Check(PyList_GetItem(p, i)))
-			print_python_bytes(PyList_GetItem(p, i));
+		printf("Element %zu: %s\n", i, l->ob_item[i]->ob_type->tp_name);
+		if (PyBytes_Check(l->ob_item[i]))
+			print_python_bytes(l->ob_item[i]);
 	}
-
 }
 
