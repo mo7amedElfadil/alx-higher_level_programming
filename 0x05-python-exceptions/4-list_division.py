733716326d660e7e6ad4fc9e98420bf54fd1c4ca
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-20 18:32:55 +0200

    mandatory

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 def catch(func, *s):
+    result = 0
     try:
-        return func(s[0][s[2]], s[1][s[2]])
+        result = func(s[0][s[2]], s[1][s[2]])
     except TypeError:
         print("wrong type")
     except ZeroDivisionError:
         print("division by 0")
     except IndexError:
         print("out of range")
-    return 0
+    finally:
+        return result
+
 
 def div(a, b):
     return a / b
 
+
 def list_division(my_list_1, my_list_2, list_length):
     result = [catch(div, my_list_1, my_list_2, i) for i in range(list_length)]
     return result
-
