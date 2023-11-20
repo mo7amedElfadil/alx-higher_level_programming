a95d889bcd089f644e5ffc5bfa78641919e3162f
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-21 01:50:04 +0200

    covert float to str

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -71,11 +71,14 @@ void print_python_float(PyObject *p)
 		return;
 	}
 	PyFloatObject *floatObj = (PyFloatObject *)p;
-	int pre = getDecimalPlaces(floatObj->ob_fval);
-	if (!pre)
-		printf("  value: %0.1f\n", floatObj->ob_fval);
-	else
-		printf("  value: %0.*g\n", pre + 1, floatObj->ob_fval);
+	/* int pre = getDecimalPlaces(floatObj->ob_fval); */
+	char *double_str = PyOS_double_to_string(floatObj->ob_fval, 'r', 0,
+			Py_DTSF_ADD_DOT_0, Py_DTST_FINITE);
+	printf("  value: %s\n", double_str);
+	/* if (!pre) */
+	/* 	printf("  value: %0.1f\n", floatObj->ob_fval); */
+	/* else */
+	/* 	printf("  value: %0.*g\n", pre + 1, floatObj->ob_fval); */
 	fflush(stdout);
 }
 
