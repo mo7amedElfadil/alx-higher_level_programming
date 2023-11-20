38b855910a3f0392399022d2153f05da4300e990
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-20 16:46:26 +0200

    initial commit + t0 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    try:
+        for i, e in enumerate(my_list):
+            if i == x:
+                i -= 1
+                break
+            print(e)
+    except Exception as ex:
+        print(ex.args[0])
+        return i
+    return i + 1
