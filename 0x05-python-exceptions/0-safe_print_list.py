1b52e896842adae490effaef8d9fbf7b148234af
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-20 16:53:32 +0200

    remove i dec

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,6 @@ def safe_print_list(my_list=[], x=0):
     try:
         for e in my_list:
             if i == x:
-                i -= 1
                 break
             i += 1
             print(e, end="")
