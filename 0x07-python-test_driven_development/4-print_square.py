1355d9aacbab065e13ad578b171d489b0e1aacc2
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-11-29 15:51:53 +0200

    updating 2 tasks

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -85,8 +85,5 @@ def print_square(size):
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
-    if (size == 0):
-        print()
-        return
     for _ in range(size):
         print("#" * size)
