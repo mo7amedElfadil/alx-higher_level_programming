ab2da5bdbd80ad89063c1d99e165d12559869bab
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-12-06 04:28:48 +0200

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -14,22 +14,29 @@ Each 10 lines and after a keyboard interruption (CTRL + C),
         format: <status code>: <number>
         status codes should be printed in ascending order
 """
-from sys import stdin
-count = 0
-lst = []
-stat = [200, 301, 400, 401, 403, 404, 405, 500]
-size = 0
-df = {}
-df = df.fromkeys(stat, 0)
-print(df)
-for line in stdin:
-    lst = line.split()
-    size += int(lst[-1])
-    if int(lst[-2]) in df:
-        df[int(lst[-2])] += 1
-        count += 1
-    if count % 10 == 0:
-        print(f"File size: {size}")
-        for k, v in df.items():
-            if v:
-                print(f"{k}: {v}")
+
+
+def print_stats():
+    """print the stats from stdin according to fmt
+    """
+    from sys import stdin
+    count = 0
+    lst = []
+    stat = [200, 301, 400, 401, 403, 404, 405, 500]
+    size = 0
+    df = {}
+    df = df.fromkeys(stat, 0)
+    for line in stdin:
+        lst = line.split()
+        size += int(lst[-1])
+        if int(lst[-2]) in df:
+            df[int(lst[-2])] += 1
+            count += 1
+        if count % 10 == 0:
+            print(f"File size: {size}")
+            for k, v in df.items():
+                if v:
+                    print(f"{k}: {v}")
+
+if __name__ == "__main__":
+    print_stats()
