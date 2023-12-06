87bc855d2c906fd184d268b773b474b084c91efb
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-12-06 02:04:08 +0200

    using find instead of is in

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,7 +14,7 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, "r", encoding="utf-8") as fp:
         lines = fp.readlines()
         for i, line in enumerate(lines):
-            if search_string in line:
+           if line.find(search_string) != -1:
                 lines.insert(i + 1, new_string)
 
     with open(filename, "w", encoding="utf-8") as fp:
