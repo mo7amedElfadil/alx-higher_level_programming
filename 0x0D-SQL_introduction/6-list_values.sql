1b2fd063f2a734f445aad14871832014037e8492
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2024-01-16 23:01:07 +0200

    5-8

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- lists all rows of the table first_table
+-- All fields should be printed
+-- The database name will be passed as an argument of the mysql command
+-- cat 6-list_values.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT * FROM first_table;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- displays the number of records with id = 89 in the table first_table
+-- cat 8-count_89.sql | mysql -hlocalhost -uroot -p hbtn_0c_0 | tail -1
+SELECT COUNT(*) FROM first_table WHERE `id`=89;
