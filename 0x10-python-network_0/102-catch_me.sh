0fc37bb2edd645e375aed71105f90ccbe3c7c19b
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-01 03:46:38 +0200

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes the server respond with a message containing You got me!
-curl -sX PUT -d "user_id=98" -L 0.0.0.0:5000/catch_me
+curl -sLX PUT -d  "user_id=98" -H "Origin: School" -e 0.0.0.0:5000/catch_me 0.0.0.0:5000/catch_me_2
