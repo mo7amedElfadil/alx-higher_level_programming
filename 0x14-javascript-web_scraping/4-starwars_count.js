a07ba23ba6a2d16c861b53ae723ed95b2da45b83
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-26 23:21:06 +0200

    testing for ubon4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (error, response, body) => {
+request(url, function (error, response, body) {
   if (error) {
     console.error('error:', error);
   } else {
