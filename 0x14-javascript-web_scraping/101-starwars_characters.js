21fc0fdc81066cf80e6bb57dc431293990d1398d
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-26 23:44:25 +0200

    using async and promises

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -10,19 +10,57 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, function (error, response, body) {
-  if (error) console.log('error:', error);
-  else {
-    if (response.statusCode === 200) {
-      const charactersUrls = JSON.parse(body).characters;
-      charactersUrls.forEach(characterUrl => {
-        request(characterUrl, function (error, response, body) {
-          if (error) console.log('error:', error);
-          else {
-            if (response.statusCode === 200) console.log(JSON.parse(body).name);
-          }
-        });
-      });
+// Fetch character name
+// Returns a promise
+// promise is resolved with the character name
+// promise is rejected with an error
+function fetchCharacter (characterUrl) {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, function (error, response, body) {
+      if (error) reject(error);
+      else {
+        if (response.statusCode === 200) resolve(JSON.parse(body).name);
+        else reject(Error('Request failed with status code ' + response.statusCode));
+      }
+    });
+  });
+}
+
+// Request characters
+// Returns a promise
+// promise is resolved with the list of characters
+// promise is rejected with an error
+function requestCharacters (url) {
+  return new Promise((resolve, reject) => {
+    request(url, function (error, response, body) {
+      if (error) reject(error);
+      else {
+        if (response.statusCode === 200) {
+          const characters = JSON.parse(body).characters;
+          resolve(characters);
+        } else {
+          reject(Error('Request failed with status code ' + response.statusCode));
+        }
+      }
+    });
+  });
+}
+
+// Main function
+// asyncronously request characters
+// for each character fetch the name
+// print the name
+// catch any error
+async function main () {
+  try {
+    const characters = await requestCharacters(url);
+    for (const character of characters) {
+      const name = await fetchCharacter(character);
+      console.log(name);
     }
+  } catch (error) {
+    console.error(error);
   }
-});
+}
+
+main();
